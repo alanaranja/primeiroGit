3f91ace4082fe15cd65b4be005eacc1eeedb7710
alanaranja <alaranjarosa@gmail.com>
2023-08-02 19:58:00 -0300

    segunda tentativa de horas para minutos

---
+++ primeirogit.py
@@ -0,0 +1,4 @@
+print('esse programinha calcula quantos minutos existem dentro das horas...')
+num1=float(input('digite o número de horas:'))
+result=num1*60
+print('o valor de', num1, 'horas corresponde a', result, 'minutos. :-)')
